87eb6c51d4e278f5ac0eccbfaacc11a782970771
tepong32 <tepong32@gmail.com>
2020-10-18 20:50:42 +0800

    added darkmode toggle ---future mods needed for colors

--- home/urls.py
+++ home/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
-# from .views import LandingPageListView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
-
-from blog.views import BlogIndexView
 
 urlpatterns = [
 	# func-based view
@@ -18,13 +15,8 @@ urlpatterns = [
 
     # get this done with pinax.announcements & pinax.templates
     # class-based detail view needed? check blog templates
-    path('announcements/<int:pk>/', views.announcement, name='announcement-detail'),
+    path('announcement/<int:pk>/', views.announcement, name='announcement-detail'),
 
-    # testing func-based views
-    path('test/', views.testing, name='testing' ),
-
-    # testing class-based views
-    # path('asdf/', LandingPageListView.as_view(), name='landingpagelistview')
 ]
 
 
